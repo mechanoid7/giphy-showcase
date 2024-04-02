b1762458b7369f7e9ab53f14a9f54d8e3ce25159
Nikita <55248870+mechanoid7@users.noreply.github.com>
2024-04-02 19:58:11 +0300

    clean, format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from "@angular/core";
+import {RouterOutlet} from "@angular/router";
+import {ListPageComponent} from "./components/list-page/list-page.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: "app-root",
+    standalone: true,
+    imports: [
+        RouterOutlet,
+    ],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.less",
 })
 export class AppComponent {
-  title = 'giphy-showcase';
+    title = "giphy-showcase";
 }
