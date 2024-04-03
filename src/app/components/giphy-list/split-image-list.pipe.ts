5289b6c1b687bfca258ed1a9ff3231951f3d0d42
Nikita <55248870+mechanoid7@users.noreply.github.com>
2024-04-03 21:42:01 +0300

    upd split pipe

--- src/app/components/giphy-list/split-image-list.pipe.ts
+++ src/app/components/giphy-list/split-image-list.pipe.ts
@@ -3,26 +3,33 @@ import {
     Pipe,
     PipeTransform,
 } from "@angular/core";
-import {GiphyImage} from "../../models/giphy.model";
+import {
+    GiphyImage,
+    GiphyImageSize,
+} from "../../models/giphy.model";
+
+const DEFAULT_GAP = 10;
 
 @Pipe({
     name: "appSplitImageList",
     standalone: true,
 })
 export class SplitImageListPipe implements PipeTransform{
-    public transform(images: GiphyImage[], width: number): GiphyImage[][] {
+    public transform(images: GiphyImage[], width: number, height: number): GiphyImage[][] {
+        const getRelativeWidth = (size: GiphyImageSize) => height * Number(size.width) / Number(size.height);
         const result: GiphyImage[][] = [];
         let currentSubArray: GiphyImage[] = [];
         let currentWidth = 0;
 
         for (const item of images) {
-            if (currentWidth + Number(item.sizes.downsized.width) <= width) {
+            const actualWidth = getRelativeWidth(item.sizes.downsized) + DEFAULT_GAP;
+            if (currentWidth + Number(actualWidth) <= width) {
                 currentSubArray.push(item);
-                currentWidth += Number(item.sizes.downsized.width);
+                currentWidth += Number(actualWidth);
             } else {
                 result.push(currentSubArray);
                 currentSubArray = [item];
-                currentWidth = Number(item.sizes.downsized.width);
+                currentWidth = Number(actualWidth);
             }
         }
 
