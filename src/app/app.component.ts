54cbc54bab0219f6291e6cdc4d5302084a6ec52a
Nikita <55248870+mechanoid7@users.noreply.github.com>
2024-04-04 01:25:29 +0300

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import {HttpClient} from "@angular/common/http";
 import {Component} from "@angular/core";
-import {RouterOutlet} from "@angular/router";
 import {MainPageComponent} from "./components/main-page/main-page.component";
 
 @Component({
     selector: "app-root",
     standalone: true,
     imports: [
-        RouterOutlet,
         MainPageComponent,
     ],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.less",
 })
 export class AppComponent {
-    title = "giphy-showcase";
 }
